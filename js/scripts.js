babb60f9b7d6c2b883ccfffa3c50ff17e7c10418
Antoniel Pereira <37280965+antdcriativo@users.noreply.github.com>
2023-01-06 16:26:23 -0300

    todoForm-js-eventos

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForma = document.querySelector("#edit-form");
+const eidtInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+
+// Funções
+
+//Eventos
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    console.log("Enviou fomr");
+});
